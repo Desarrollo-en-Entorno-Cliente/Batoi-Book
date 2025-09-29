902fc7fe697587597d5c1c6b79d183e51e456103
Pedro <pedrocorreo@gmail.com>
2025-09-29 18:20:38 +0200

    Commit Estructura principal

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from '/image.png'
+
+import '/src/functions.js'
+
 
 document.querySelector('#app').innerHTML = `
+<center>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src=".${javascriptLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <h1>BatoiBooks</h1>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Abre la consola para ver el resultado
     </p>
   </div>
+  </center>
 `
 
-setupCounter(document.querySelector('#counter'))
